370919ac858928a5a35cc667d0851d9e59b68baa
JonDD-D <dengjohn123@163.com>
2024-09-09 20:27:27 -0400

    added more comments and explain the main function to be called

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         FizzBuzz fizzBuzz = new FizzBuzz();
+        /* For the main thing, we can access it through a different way, by claiming the main(args)
+        as shown in following
+         */
+        fizzBuzz.main(args);
+        // THIS is for showing calling the function outside FizzBuzz.java class as mentioned inside
+        // README.md file
         int i = 100;
-        while (i<1233){
+        while (i<120){
             i = fizzBuzz.doFizzBuzz(i);
         }
     }
