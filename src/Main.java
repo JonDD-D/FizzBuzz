ebdc8e372d5a0d94621b63e88cce35c8075d0d96
JonDD-D <dengjohn123@163.com>
2024-09-09 20:24:20 -0400

    this is the change- 123 to 1233

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         FizzBuzz fizzBuzz = new FizzBuzz();
         int i = 100;
-        while (i<123){
+        while (i<1233){
             i = fizzBuzz.doFizzBuzz(i);
         }
     }
